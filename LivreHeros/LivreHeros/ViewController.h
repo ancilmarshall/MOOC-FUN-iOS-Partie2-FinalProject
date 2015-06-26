d9b963b31e99543c5eed2dbabf72c82ec358df5c
Ancil <ancilm2000@yahoo.com>
2015-06-26 10:01:39 +0200

    Create empty project and commit to github

---
+++ LivreHeros/LivreHeros/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LivreHeros
+//
+//  Created by Ancil on 6/26/15.
+//  Copyright (c) 2015 Ancil Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LivreHeros/LivreHeros/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LivreHeros
+//
+//  Created by Ancil on 6/26/15.
+//  Copyright (c) 2015 Ancil Marshall. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
